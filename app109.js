0ce1a8a0de25613c7d7afb7efc462c71b82e7df1
Mohen sunder. K <122079939+mohensunder@users.noreply.github.com>
2024-01-30 16:05:40 +0530

    Add files via upload

---
+++ app109.js
@@ -0,0 +1,9 @@
+var a=[10,20,"mohen","sunder",34,12.5,"A"];
+console.log(a.splice(0,1));
+console.log(a);
+// Remove the first element
+
+var a=[10,20,"mohen","sunder",34,12.5,"A"];
+console.log(a.splice(0,3));
+console.log(a);
+// starting index delete count
